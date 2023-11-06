001332c7fb53f6ebd70a0386c50745d7eb79d8b5
Igor Serafim <igor.serafim@nolifretes.com.br>
2023-11-06 13:59:58 -0300

    chore authenticate jwt and /me

--- src/routes/authenticate.ts
+++ src/routes/authenticate.ts
@@ -1,16 +1,42 @@
+import { compare, hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { checkJwt } from '../middlewares/check-jwt'
+
+interface User {
+  id: string
+  name: string
+  email: string
+  password: string
+}
+
+const users: User[] = []
 
 export async function authenticateRouter(app: FastifyInstance) {
   app.post('/signup', async (req, res) => {
     const bodySchema = z.object({
+      name: z.string(),
       email: z.string().email(),
       password: z.string().length(6),
     })
 
     const body = bodySchema.parse(req.body)
 
-    console.log(body)
+    const emailAlreadyExists = users.some((user) => user.email === body.email)
+
+    if (emailAlreadyExists) {
+      return res.status(409).send({ message: 'Email already exists' })
+    }
+
+    const password = await hash(body.password, 6)
+
+    users.push({
+      id: randomUUID(),
+      email: body.email,
+      name: body.name,
+      password,
+    })
 
     return res.status(201).send()
   })
@@ -23,8 +49,45 @@ export async function authenticateRouter(app: FastifyInstance) {
 
     const body = bodySchema.parse(req.body)
 
-    console.log(body)
+    const user = users.find((user) => user.email === body.email)
 
-    return res.status(200).send({ user: 'User' })
+    if (!user) {
+      return res.status(401).send({ message: 'Invalid credentials' })
+    }
+
+    const passwordMatches = await compare(body.password, user.password)
+
+    if (!passwordMatches) {
+      return res.status(401).send({ message: 'Invalid credentials' })
+    }
+
+    const token = await res.jwtSign({}, { sign: { sub: user.id } })
+
+    const refreshToken = await res.jwtSign(
+      {},
+      { sign: { sub: user.id, expiresIn: '5m' } },
+    )
+
+    return res
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token })
+  })
+
+  app.get('/me', { onRequest: [checkJwt] }, (req, res) => {
+    const userId = req.user.sub
+
+    const user = users.find((user) => user.id === userId)
+
+    if (!user) {
+      return res.status(400).send({ message: 'User not found.' })
+    }
+
+    return res.status(200).send({ user: { ...user, password: undefined } })
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,37 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import fastifyJwt from '@fastify/jwt'
 import { authenticateRouter } from './routes/authenticate'
 import { mealsRouter } from './routes/meals'
 import { metricsRouter } from './routes/metrics'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
 app.register(cookie)
 
+app.register(fastifyJwt, {
+  secret: 'secret54321',
+  cookie: { cookieName: 'refreshToken', signed: false },
+  sign: { expiresIn: '2m' },
+})
+
 app.register(authenticateRouter, { prefix: 'user' })
 app.register(mealsRouter, { prefix: 'meals' })
 app.register(metricsRouter, { prefix: 'metrics' })
+
+app.setErrorHandler((error, _, res) => {
+  if (error instanceof ZodError) {
+    return res.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  return res.status(500).send({
+    message: 'Internal server error.',
+  })
+})
 
 app
   .listen({
